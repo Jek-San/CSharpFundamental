10855e97152d5752bb42eb1ab8208a3650ff85ba
Muhammad Ihsan <ihsanzack@gmailcom>
2022-12-23 15:28:51 +0700

    newwwwwwwwwwww

--- Bengkel/Program.cs
+++ Bengkel/Program.cs
@@ -11,21 +11,48 @@ namespace Bengkel
     {
         static void Main(string[] args)
         {
-           
+
+
+            Console.Write("Input :");
+            string input = Console.ReadLine();
+            string[] inputSplit = input.Split(',');
+            List<int> inputInt = Array.ConvertAll(inputSplit, int.Parse).ToList();
+            List<int> totalKecil = new List<int>();
+            List<int> totalBesar = new List<int>();
+            int length = inputInt.Count;
+
+            inputInt.Sort();
+            Console.Write("Angka Urut :");
+            foreach (var item in inputInt)
+            {
+                Console.WriteLine($"{item} ");
+            }
             
-            Console.Clear();
-            Console.Write("q : ");
-            int q = int.Parse(Console.ReadLine());
-            Console.WriteLine("input kata: ");
-            char[] kata = Console.ReadLine().ToCharArray();
+           
+            for (int i = 0; i < 3; i++) {
+                int temp = inputInt[i];
+                totalKecil.Add(temp);
+            }
+            inputInt.Reverse();
+            for (int i = 0; i < 3; i++)
+            {
+                int temp = inputInt[i];
+                totalBesar.Add(temp);
+            }
 
-            string s = "hackerrank";
-            char[] sArr = s.ToCharArray();
+            Console.Write("Outout : ");
+            Console.WriteLine($"total 3 Angka terbesar : {totalBesar.Sum()}");
+            Console.WriteLine($"         total 3 Angka terkecil : {totalKecil.Sum()}");
 
-            List<char> kataList = new List<char>();
 
+            /*Console.Write("Angka Urut :");
+            
+            string urut = Console.ReadLine();
+            string[] urutSplit = urut.Split(' ');
+            int[] urutInt = Array.ConvertAll(urutSplit, int.Parse);
+*/
 
-            Console.ReadLine();
+            
             
         }
     }
