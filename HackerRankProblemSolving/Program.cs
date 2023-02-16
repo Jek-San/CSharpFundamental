a7c8b486a0e80836cb72ecd93af5acb0ba10ada8
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-02-17 06:35:28 +0700

    Hackkerrank

--- HackerRankProblemSolving/Program.cs
+++ HackerRankProblemSolving/Program.cs
@@ -8,40 +8,185 @@ namespace HackerRankProblemSolving
     {
         static void Main(string[] args)
         {
-            //https://www.hackerrank.com/challenges/kangaroo/
-            int x1 = 1571, v1 = 4240, x2 = 9023, v2 = 4234;
-            string result = "NO";
-            if ((v1 > v2 && x1 > x2) || (v2 > v1 && x2 > x1))
+            string temp = "";
+            string value1 = "value 1";
+            string value2 = "value 2";
+            Console.WriteLine("Before tukar");
+            Console.WriteLine($"valu1 = {value1}");
+            Console.WriteLine($"value 2 = {value2}");
+
+            temp = value1;
+            value1= value2;
+            value2 = temp;
+            Console.WriteLine("After tukar");
+            Console.WriteLine($"valu1 = {value1}");
+            Console.WriteLine($"value 2 = {value2}");
+
+        }
+
+        static void ReverseString()
+        {
+            string input = "Halo Guus";
+            var oke = input.ToCharArray();
+            var reverse = oke.Reverse().ToArray();
+            var result = new string(reverse);
+            Console.WriteLine(result);
+
+            string resultBaru = "";
+            var inputArray = input.ToCharArray();
+            int len = inputArray.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                resultBaru += inputArray[i];
+            }
+            Console.WriteLine(resultBaru);
+        }
+        static void TestInput()
+        {
+            Console.WriteLine("Masukkan No Loker yang ingin anda pilih : ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Lantai ke {Test(input)}");
+        }
+        static int Test(int loker)
+        {
+            int lokerKe = 0;
+            int batas = 5;
+            int lantai; 
+            int pola = 0;
+
+            for (lantai = 1; lantai < int.MaxValue; lantai++)
+            {
+                for (int i = 0; i < batas + pola; i++)
+                {
+                    lokerKe++;
+                    if (lokerKe == loker)
+                    {
+                        return lantai;
+                    }
+                }
+                pola++;
+                if (pola > 3)
+                {
+                    pola = 1;
+                }
+            }
+            return lantai;
+            
+        }
+        static void ProblemSplving_MinMaxScoreBasketBall()
+        {
+            //https://www.hackerrank.com/challenges/breaking-best-and-worst-records/
+            List<int> scores = new List<int> { 0, 9, 3, 10, 2, 20 };
+            List<int> result = new List<int>();
+            int minChange = 0;
+            int maxChange = 0;
+            int len = scores.Count;
+            int min = 0;
+            int max = 0;
+            int now = 0;
+            max = scores[0];
+            min = scores[0];
+            for (int i = 0; i < len; i++)
             {
 
+
+                if (scores[i] > max)
+                {
+                    max = scores[i];
+                    maxChange++;
+                }
+                else
+                {
+                    if (scores[i] < min)
+                    {
+                        min = scores[i];
+                        minChange++;
+
+
+                    }
+                }
+
+            }
+            result.Add(maxChange); result.Add(minChange);
+            Console.WriteLine($"{maxChange}, {minChange}");
+        }
+        static void ProblemSolving_Grades()
+        {
+            //https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+            List<int> grades = new List<int> { 73, 67, 38, 33 };
+            List<int> result = new List<int>();
+            int len = grades.Count;
+            for (int i = 0; i < len; i++)
+            {
+                int nilai = grades[i];
+                int cekTambah = 0;
+
+                if (nilai % 5 == 0)
+                {
+                    result.Add(nilai);
+                }
+                else
+                {
+                    while (nilai % 5 != 0)
+                    {
+                        cekTambah++;
+                        nilai += 1;
+                    }
+                    if (cekTambah <= 2)
+                    {
+                        if (nilai >= 40)
+                        {
+                            result.Add(nilai);
+
+                        }
+                        else
+                        {
+                            result.Add(grades[i]);
+                        }
+                    }
+                    else
+                    {
+                        result.Add(grades[i]);
+                    }
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void ProblemSolving_KangorooJump()
+        {
+            int x1 = 4523, v1 = 8092, x2 = 9419, v2 = 8076;
+            string result = "NO";
+            if (v1 > v2 && x1 > x2 || v2 > v1 && x2 > x1)
+            {
                 Console.WriteLine(result);
             }
             else
             {
                 int position1 = x1;
                 int position2 = x2;
-                int batas = 0;
-                while (position1 == position2 || position1 >= 10000 || position2 >= 10000 && batas ==4)
+                while (position1 != position2)
                 {
-                    batas++;
                     position1 += v1;
                     position2 += v2;
+                    Console.WriteLine(position1 + " " + position2);
                     if (position1 == position2)
                     {
                         result = "YES";
+                        break;
                     }
 
+                    if (position1 >= int.MaxValue || position2 >= int.MaxValue)
+                    {
+                        break;
+                    }
                 }
-
             }
             Console.WriteLine(result);
         }
-
-
-    
-
-
-    static void ProbleSolving_AppleAndOrange()
+        static void ProbleSolving_AppleAndOrange()
         {
             //https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
             int s = 7, t = 10, a = 4, b = 12;
@@ -101,7 +246,7 @@ namespace HackerRankProblemSolving
         }
         static void ProbleSolvingTallesCandles()
         {
-        //https://www.hackerrank.com/challenges/time-conversion
+            //https://www.hackerrank.com/challenges/time-conversion
             List<int> candles = new List<int> { 4, 4, 1, 3 };
             int tallest = candles.Max();
             int count = candles.Count(x => x == tallest);
